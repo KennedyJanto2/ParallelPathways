34f8f061033750c1990dc202c224e4fbfb3dd105
cvoneul <cvoneul@gmail.com>
2023-12-13 22:39:25 -0800

    added die animation

--- Parallel Pathways/Assets/Scripts/Die.cs
+++ Parallel Pathways/Assets/Scripts/Die.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Die : MonoBehaviour
 {
     public float boundary;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,7 +17,7 @@ public class Die : MonoBehaviour
     {
         if(transform.position.y < boundary)
         {
-            respawn();
+            die();
         }
     }
 
@@ -27,14 +28,29 @@ public class Die : MonoBehaviour
     
         if(col == "Projectile" || col == "Laser")
         {
-            respawn();
+            die();
         }
         
     }
 
+
+
+    void die()
+    {
+        StartCoroutine(dieHelper());
+    }
+
+
+    IEnumerator dieHelper() 
+    {
+        respawn();
+        anim.SetTrigger("die");
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorClipInfo(0).Length);
+        anim.SetTrigger("idle");
+    }
+
     public void respawn() {
         transform.position = new Vector3(0, 0, 0);
-        //insert die animation here
         GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
     }
 }
